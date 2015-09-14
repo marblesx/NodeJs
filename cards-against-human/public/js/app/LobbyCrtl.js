f893ba267028099ce30f56c325f9242dc2be5f03
Wicki <kotwicki1990@gmail.com>
2015-09-14 16:33:59 -0400

    broke out controllers to files

---
+++ cards-against-human/public/js/app/LobbyCrtl.js
@@ -0,0 +1,45 @@
+
+var ctrl = angular.module('myApp.controllers', []);
+
+ctrl.controller('LobbyCtrl', function($scope, $location, GameService) {
+        console.info('LobbyCtrl loaded');
+        var socket;
+
+        $scope.availableGames = [];
+        $scope.creatingGame = false;
+        $scope.gameSvc = GameService;
+
+        $scope.getGames = function() {
+            GameService.getGames()
+                .then(function(success) {
+                    var games = success.data;
+                    console.info('getGames returned ' + games.length + ' items');
+                    $scope.availableGames = games;
+            });
+        };
+
+        function initSocket() {
+            socket = io.connect('/lobby');
+            if(socket.socket.connected){
+                $scope.getGames();
+            }
+            socket.on('connect', function() {
+                console.info('lobby socket connect');
+            });
+
+            socket.on('lobbyJoin', function(gameList) {
+                console.info('lobbySocket: lobbyJoin');
+                $scope.availableGames = gameList;
+                $scope.$apply();
+            });
+
+            socket.on('gameAdded', function(gameList) {
+                console.info('gameAdded');
+                console.info(gameList);
+                $scope.availableGames = gameList;
+                $scope.$apply();
+            });
+        }
+        initSocket();
+        $scope.$emit('enterLobby');
+    });
